84494f68274e03d7b5976d34b334ca46e4747c4f
Clara SAUNIER <clara.saunier@ens.uvsq.fr>
2021-11-24 15:16:35 +0100

    TD

--- exercises/TD05_gui/prise en main.py
+++ exercises/TD05_gui/prise en main.py
@@ -14,7 +14,7 @@ canvas.create_line(y, x0, y, x1)
 canvas.create_oval(y - 50, x0 + 50, y + 50, x0 - 50)
 canvas.create_oval(y - 50, x1 + 50, y + 50, x1 - 50)
 canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
-    
+
 # Fin de votre code
 
 canvas.grid()
