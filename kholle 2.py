1f6d2f87dd417c9127f1bb441b23907272312f8f
Clara SAUNIER <clara.saunier@ens.uvsq.fr>
2022-03-09 16:09:57 +0100

    TD 9/3

---
+++ kholle 2.py
@@ -0,0 +1,40 @@
+import random
+
+liste=[]
+liste1=[]
+nbr_dans_liste=int(input("donner un nombre"))
+for i in range(nbr_dans_liste):
+    liste.append(random.randint(0,10))
+    liste1.append(random.randint(0,10))
+print(liste,liste1)
+
+
+
+import random
+
+liste2 =[]
+n=int(input("donner un nombre"))
+def list():
+    for i in range(n):
+        liste.append(random.randint(10,99))
+    return(liste)
+
+
+print(list())
+
+
+
+
+
+import random
+l=[list()]
+l1=[]
+
+def multiplierlaliste():
+    for i in range(n):
+        l1.append(l[i]*3)
+    return(l1)
+
+print (l)
+print(l1)
+
